4edd756fb69c8d5d9490d660762f70aebb532609
krylon <>
2017-09-06 11:20:45 +0000

    Ich fange noch mal von vorn an und versuche den Parser mit gocc zu bauen.
    FossilOrigin-Name: bd3879b0c2e7087188c51f6631f2c5c776db3ce0036a85f74158f2959c96f208

---
+++ lexer/lexer.go
@@ -0,0 +1,7 @@
+// /home/krylon/go/src/krylisp/lexer/lexer.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 06. 09. 2017 by Benjamin Walkenhorst
+// (c) 2017 Benjamin Walkenhorst
+// Time-stamp: <2017-09-06 10:27:07 krylon>
+
+package lexer
--- token/token.go
+++ token/token.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 04. 09. 2017 by Benjamin Walkenhorst
 // (c) 2017 Benjamin Walkenhorst
-// Time-stamp: <2017-09-04 20:04:12 krylon>
+// Time-stamp: <2017-09-06 10:15:09 krylon>
 //
 // Montag, 04. 09. 2017, 20:03
 // Ich bin mir noch nicht sicher, wie weit sich diese Datei gegenüber dem
@@ -24,8 +24,10 @@ const (
 	IDENT     = "IDENT"
 	SYMBOL    = "SYMBOL"
 	LAMBDA    = "LAMBDA"
+	DEFUN     = "DEFUN"
+	BEGIN     = "BEGIN"
 	NIL       = "NIL"
-	PLUS      = "PLUS"
+	PLUS      = "+"
 	MINUS     = "-"
 	BANG      = "!"
 	ASTERISK  = "*"
@@ -48,9 +50,12 @@ const (
 
 var keywords = map[string]TokenType{
 	"lambda": LAMBDA,
+	"defun":  DEFUN,
+	"begin":  BEGIN,
 	"if":     IF,
 	"t":      TRUE,
 	"nil":    NIL,
+	"+":      PLUS,
 }
 
 func LookupIdent(ident string) TokenType {
