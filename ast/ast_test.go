a185efd83ca9d5bc3b97ed44a1b62411d71559a4
krylon <>
2017-09-06 11:21:25 +0000

    Alte Parser-Dateien entfernt.
    FossilOrigin-Name: 9ac0127aceae585d44da2e2e8baab8ce511f4ec3575dc038207288be245ba4b5

--- ast/ast_test.go
+++
@@ -1,7 +0,0 @@
-// /home/krylon/go/src/krylisp/ast/ast_test.go
-// -*- mode: go; coding: utf-8; -*-
-// Created on 06. 09. 2017 by Benjamin Walkenhorst
-// (c) 2017 Benjamin Walkenhorst
-// Time-stamp: <2017-09-06 10:31:12 krylon>
-
-package ast
