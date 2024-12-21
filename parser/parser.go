1cf2961b27f7c1d02575473fc80aaeae7d8ee052
Benjamin Walkenhorst <krylon@posteo.de>
2024-12-21 23:18:35 +0100

    Add interface parser.LispValue

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,13 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 21. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-21 21:59:55 krylon>
+// Time-stamp: <2024-12-21 23:03:09 krylon>
 
 // Package parser provides the ... parser.
 package parser
+
+import "github.com/blicero/krylisp/types"
+
+type LispValue interface {
+	Type() types.Type
+}
