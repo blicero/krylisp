d9db77c1b7aade36a4d6115b46bbfe81d779cf18
Benjamin Walkenhorst <krylon@posteo.de>
2024-12-22 01:43:48 +0100

    Work on parser

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 21. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-21 23:03:09 krylon>
+// Time-stamp: <2024-12-22 01:29:29 krylon>
 
 // Package parser provides the ... parser.
 package parser
@@ -11,4 +11,33 @@ import "github.com/blicero/krylisp/types"
 
 type LispValue interface {
 	Type() types.Type
+}
+
+type Symbol struct {
+	Symbol string `parser:"@Symbol"`
+}
+
+func (s *Symbol) Type() types.Type { return types.Symbol }
+
+type String struct {
+	String string `parser:"@String"`
+}
+
+func (s *String) Type() types.Type { return types.String }
+
+type Integer struct {
+	Integer int64 `parser:"@Integer"`
+}
+
+func (i *Integer) Type() types.Type { return types.Integer }
+
+type Float struct {
+	Float float64 `parse:"@Float"`
+}
+
+func (f *Float) Type() types.Type { return types.Float }
+
+type ConsCell struct {
+	Car LispValue `parse:"@Car"`
+	Cdr LispValue `parse:"@Cdr"`
 }
--- types/types.go
+++ types/types.go
@@ -2,10 +2,12 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 21. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-21 21:59:42 krylon>
+// Time-stamp: <2024-12-22 01:21:16 krylon>
 
 // Package types provides symbolic constants to identify the types used in kryLisp
 package types
+
+//go:generate stringer -type=Type
 
 // Type identifies the type of a Lisp value
 type Type uint8
