1f0c7ec227248b48116a641b1bc09258e856c683
Benjamin Walkenhorst <krylon@posteo.de>
2025-02-15 18:10:50 +0100

    A few more tests of looking up bindings

--- interpreter/01_env_test.go
+++ interpreter/01_env_test.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 15. 02. 2025 by Benjamin Walkenhorst
 // (c) 2025 Benjamin Walkenhorst
-// Time-stamp: <2025-02-15 15:56:51 krylon>
+// Time-stamp: <2025-02-15 17:49:36 krylon>
 
 package interpreter
 
@@ -27,11 +27,15 @@ func TestLookupSimple(t *testing.T) {
 	var env = Environment{
 		Bindings: map[parser.Symbol]parser.LispValue{
 			sym("zero"): parser.Integer{Int: 0},
+			sym("name"): parser.String{Str: "Odysseus"},
+			sym("age"):  parser.Integer{Int: 42},
 		},
 	}
 
 	var cases = []testCase{
-		{key: sym("ZAPPELWURST"), expectOK: false},
+		{key: sym("zappelwurst"), expectOK: false},
+		{key: sym("zero"), expectOK: true, expectedValue: parser.Integer{Int: 0}},
+		{key: sym("name"), expectOK: true, expectedValue: parser.String{Str: "Odysseus"}},
 	}
 
 	for _, c := range cases {
@@ -53,4 +57,67 @@ func TestLookupSimple(t *testing.T) {
 		}
 	}
 
-}
+} // func TestLookupSimple(t *testing.T)
+
+func TestLookupScoped(t *testing.T) {
+	var env = Environment{
+		Parent: &Environment{
+			Bindings: map[parser.Symbol]parser.LispValue{
+				sym("x"):      parser.Integer{Int: 10},
+				sym("temp"):   parser.Integer{Int: 42},
+				sym("lambda"): parser.Integer{Int: 83},
+			},
+		},
+		Bindings: map[parser.Symbol]parser.LispValue{
+			sym("y"):    parser.Integer{Int: 23},
+			sym("temp"): parser.Integer{Int: 109},
+		},
+	}
+
+	type testCase struct {
+		key           parser.Symbol
+		expectOK      bool
+		expectedValue parser.LispValue
+	}
+
+	var cases = []testCase{
+		{
+			key:           sym("y"),
+			expectOK:      true,
+			expectedValue: parser.Integer{Int: 23},
+		},
+		{
+			key:           sym("x"),
+			expectOK:      true,
+			expectedValue: parser.Integer{Int: 10},
+		},
+		{
+			key:           sym("temp"),
+			expectOK:      true,
+			expectedValue: parser.Integer{Int: 109},
+		},
+		{
+			key: sym("nothing"),
+		},
+	}
+
+	for _, c := range cases {
+		var (
+			ok  bool
+			val parser.LispValue
+		)
+
+		if val, ok = env.Lookup(c.key); !ok {
+			if c.expectOK {
+				t.Errorf("Failed to lookup %s in environment",
+					c.key)
+				continue
+			}
+		} else if !val.Equal(c.expectedValue) {
+			t.Errorf("Unexpected value for %s: %s (expected %s)",
+				c.key,
+				val,
+				c.expectedValue)
+		}
+	}
+} // func TestLookupScoped(t *testing.T)
